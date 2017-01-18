76e4b6d83da4edbf73fbc30f5e3c9a5aea00520a
geetha-hortonworks <ganne@hortonworks.com>
2017-01-18 10:33:45 -0800

    hbase->hive sample scripts

---
+++ scripts/davita-hbase.sql
@@ -0,0 +1,8 @@
+create ‘davita_demo’,‘dialysisdetails’ 
+
+put 'davita_demo','eid01', 'dialysisdetails:name','hemodialysis'
+put 'davita_demo','eid01', 'dialysisdetails:equipment','NXTStage'
+put 'davita_demo','eid02', 'dialysisdetails:name','peritoneal'
+put 'davita_demo','eid02', 'dialysisdetails:equipment','Fresinius29'
+put 'davita_demo','eid03', 'dialysisdetails:name','noctural'
+put 'davita_demo','eid03', 'dialysisdetails:equipment','Copalartar'
